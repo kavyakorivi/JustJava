8b216fbaa7ba8f52a36569baa5fd1a16aea83662
kavyakorivi <kavyakorivi005@gmail.com>
2024-02-29 13:37:52 -0600

    For loop

---
+++ src/JavaAssignments/For.java
@@ -0,0 +1,13 @@
+package JavaAssignments;
+
+
+public class For {
+    public static void main(String[] args) {
+        for(int i=1;i<25;i++){
+            if(i%5==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
